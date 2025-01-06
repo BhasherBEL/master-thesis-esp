7ee6da52e964fec2b1bfd6b7c22e4642df1a9170
Brieuc Dubois <git@bhasher.com>
2025-01-06 20:53:39 +0100

    MQTT publish devices

--- src/esp.ino
+++ src/esp.ino
@@ -2,9 +2,11 @@
 #include "BLEServer.h"
 #include "BLEUtils.h"
 #include "BLEBeacon.h"
+#include "BLEScan.h"
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #include "credentials.h"
 
@@ -20,8 +22,10 @@ bool isWiFiConnected = false;
 //#define BEACON_MINOR 49494  // red
 //#define BEACON_MINOR 49495  // blue
 #define BEACON_MINOR 49496  // green
+#define BEACON_SCAN_TIME 5
 
 BLEAdvertising *pAdvertising;
+BLEScan* pBLEScan;
 
 #define MQTT_CLIENT_ID "ESP32_49496"
 #define MQTT_RECONNECT_DELAY 5000
@@ -92,6 +96,66 @@ void blinkSym(int duration) {
 void blinkSymN(int duration, int n) {
 	blinkN(duration, duration, n);
 }
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    StaticJsonDocument<200> doc;
+    doc["rssi"] = advertisedDevice.getRSSI();
+    doc["mac"] = advertisedDevice.getAddress().toString().c_str();
+    doc["timestamp"] = millis();
+    
+    bool isIBeacon = false;
+    if (advertisedDevice.haveManufacturerData()) {
+      std::string strManufacturerData = advertisedDevice.getManufacturerData();
+      uint8_t cManufacturerData[100];
+      strManufacturerData.copy((char *)cManufacturerData, strManufacturerData.length(), 0);
+
+      // Check if it's an iBeacon (Apple's manufacturer ID and iBeacon data length)
+      if (strManufacturerData.length() == 25 && cManufacturerData[0] == 0x4C && cManufacturerData[1] == 0x00) {
+        BLEBeacon oBeacon = BLEBeacon();
+        oBeacon.setData(strManufacturerData);
+        isIBeacon = true;
+        
+        doc["uuid"] = oBeacon.getProximityUUID().toString().c_str();
+        doc["major"] = oBeacon.getMajor();
+        doc["minor"] = oBeacon.getMinor();
+      }
+    }
+
+				// Add device name if available
+				if (advertisedDevice.haveName()) {
+				  doc["name"] = advertisedDevice.getName().c_str();
+				}
+
+				char jsonBuffer[200];
+				serializeJson(doc, jsonBuffer);
+				
+				//Serial.print("Found device: ");
+				//Serial.print(jsonBuffer);
+				//Serial.print(" ... ");
+				
+				if (mqttClient.connected()) {
+				  char topic[100];
+				  if (isIBeacon && strcmp(doc["uuid"], BEACON_UUID) == 0) {
+				    // Our iBeacon
+				    snprintf(topic, sizeof(topic), "ibeacon/beacons/%d", doc["minor"].as<int>());
+				  } else {
+				    // Any other device (including other iBeacons)
+				    snprintf(topic, sizeof(topic), "ibeacon/others/%s", 
+				      advertisedDevice.getAddress().toString().c_str());
+				  }
+				  if(mqttClient.publish(topic, jsonBuffer)) {
+				  //Serial.println("Published!");
+					} else {
+						//Serial.print("Failed to publish (");
+						//Serial.print(mqttClient.state());
+						//Serial.println(")");
+					}
+				} else {
+				  //Serial.println("Not published");
+    }
+  }
+};
 
 
 void connectToWiFi() {
@@ -167,16 +231,18 @@ void setup() {
 	BLEDevice::init("ESP32 as iBeacon");
 
 	BLEServer *pServer = BLEDevice::createServer(); 
-	
 	pAdvertising = BLEDevice::getAdvertising();
-	
   BLEDevice::startAdvertising();
-
   setBeacon();
-
   pAdvertising->start();
-
   Serial.println("Advertizing started...");
+
+	pBLEScan = BLEDevice::getScan();
+	pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+	pBLEScan->setActiveScan(true);
+	pBLEScan->setInterval(100);
+	pBLEScan->setWindow(99);
+  Serial.println("Scanning started...");
 
 	pinMode(LED, OUTPUT);
 
@@ -195,6 +261,10 @@ void loop() {
 		connectToMqtt();
   } else {
     mqttClient.loop();
+
+		BLEScanResults foundDevices = pBLEScan->start(BEACON_SCAN_TIME, false);
+		pBLEScan->clearResults();
+
 		blink(100, 4900);
   }
 }
